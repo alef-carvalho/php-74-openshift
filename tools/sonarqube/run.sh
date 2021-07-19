66eb382ac4779990ea86cf249005fd78276320a9
alefcarvalho <alef.webdev@gmail.com>
2021-07-19 17:21:48 -0400

    wip

--- tools/sonarqube/run.sh
+++ tools/sonarqube/run.sh
@@ -6,10 +6,4 @@ if [ "${1:0:1}" != '-' ]; then
   exec "$@"
 fi
 
-exec java -jar lib/sonar-application-$SONAR_VERSION.jar \
-  -Dsonar.log.console=true \
-  -Dsonar.jdbc.username="$SONARQUBE_JDBC_USERNAME" \
-  -Dsonar.jdbc.password="$SONARQUBE_JDBC_PASSWORD" \
-  -Dsonar.jdbc.url="$SONARQUBE_JDBC_URL" \
-  -Dsonar.web.javaAdditionalOpts="$SONARQUBE_WEB_JVM_OPTS -Djava.security.egd=file:/dev/./urandom" \
-  "$@"
+exec "${SONARQUBE_HOME}/bin/linux-x86-64/sonar.sh"
