ac13e85866dc5a6b59cd9905b9116eff3e03b9a5
alefcarvalho <alef.webdev@gmail.com>
2021-07-19 18:45:59 -0400

    wip

--- tools/sonarqube/scripts/run.sh
+++ tools/sonarqube/scripts/run.sh
@@ -6,6 +6,4 @@ if [ "${1:0:1}" != '-' ]; then
   exec "$@"
 fi
 
-exec while true; do sleep 10 break; done
-
-#"${SONARQUBE_HOME}/bin/linux-x86-64/sonar.sh"
+exec ${SONARQUBE_HOME}/bin/linux-x86-64/sonar.sh
