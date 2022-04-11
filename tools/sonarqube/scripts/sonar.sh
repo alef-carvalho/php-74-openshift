ac8b333d146a4a3cd56efee759847870976ca071
Alef Carvalho <alef.webdev@gmail.com>
2022-04-11 10:05:48 -0400

    wip: add sonar variables

--- tools/sonarqube/scripts/sonar.sh
+++ tools/sonarqube/scripts/sonar.sh
@@ -1,6 +1,9 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 echo "UPDATING SONAR CONFIG FROM ENVIRONMENT VARIABLES"
 
 # update configuration file
 cd ${SONARQUBE_HOME}/conf && envsubst < sonar.properties > sonar.properties
+
+# run sonar
+exec java -jar lib/sonar-application-"${SONAR_VERSION}".jar -Dsonar.log.console=true "$@"
